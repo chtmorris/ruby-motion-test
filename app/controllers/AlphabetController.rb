22155ce5e0b7ec2881997da5c15207090c65a1c5
Screen Goose <sarrieschaz@hotmail.com>
2014-06-12 19:22:16 +0800

    First commit

---
+++ app/controllers/AlphabetController.rb
@@ -0,0 +1,31 @@
+class AlphabetController < UIViewController
+  def viewDidLoad
+    super
+
+    self.title = "Alphabet"
+
+    @table = UITableView.alloc.initWithFrame(self.view.bounds)
+    self.view.addSubview @table
+
+    @table.dataSource = self
+    @data = ("A".."Z").to_a
+  end
+
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+
+    # put your data in the cell
+    cell.textLabel.text = @data[indexPath.row]
+    
+    cell
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    @data.count
+  end
+
+end
